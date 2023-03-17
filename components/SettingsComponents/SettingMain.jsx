45b7b05e6ce03b6aa8d87cb54654f963223f17f7
Sagar <sagarlimbu787@gmail.com>
2023-03-17 19:29:33 +0545

    initial committ

---
+++ components/SettingsComponents/SettingMain.jsx
@@ -0,0 +1,72 @@
+import { motion, AnimatePresence } from "framer-motion";
+
+import ContactBtn from "../UI Component/ContactBtn";
+import CoffeBtn from "../UI Component/CoffeBtn";
+import GithubBtn from "../UI Component/GithubBtn";
+import SetEmpty from "../UI Component/SetEmpty";
+import SetSample from "../UI Component/SetSample";
+import TemplateSwitcher from "../UI Component/TemplateSwitcher";
+
+const SettingMain = () => {
+  return (
+    <AnimatePresence>
+      <motion.div layout className="cardStyle">
+        <h1 className="text-2xl font-bold">CV Builder</h1>
+        <div className="mt-5">
+          <p>
+          The CV Generator is a user-friendly tool that enables students to create a CV that highlights their strengths, skills, and achievements in a professional and concise manner. It offers a range of customizable templates and designs that enable students to create a CV that reflects their unique personality and style. <br/><br/>
+
+          The CV Generator also includes a range of features that make it easy for students to create a CV that stands out from the rest. These features include the ability to add a profile picture, highlight key achievements and accomplishments, and customize the layout and design of the CV.
+          </p>
+          <div className="bg-sky-50 border-2 border-sky-700/50 px-2 mt-4 mb-2 pt-4 py-2 rounded-xl">
+            <h1 className="text-sky-900 text-xl font-bold">Before using</h1>
+            <ol className="list-none mt-2 text-sky-900 space-y-2">
+              <li>
+                ● The <strong>Reset</strong> button and the
+                <strong> Fill Sample Data </strong>
+                button will clear all the changes you have made and you cannot
+                undo them.
+              </li>
+              <li>
+                ● <strong>Contact Us</strong> if you encounter any problems.
+              </li>
+              <li>
+                ● Sometimes checkboxes don't work as they should.
+                <strong> Just click twice for it to work properly.</strong>
+              </li>
+            </ol>
+          </div>
+        </div>
+        <div className="flex flex-col mt-4">
+          <div className="flex space-x-2 ">
+            <SetEmpty />
+            <SetSample />
+          </div>
+          <motion.div
+            layout
+            className="w-44 h-1 mx-auto mt-2 bg-gray-200 rounded-full"
+          />
+          <div className="flex space-x-2 mt-2">
+            <ContactBtn />
+
+            {/* <GithubBtn /> */}
+          </div>
+          {/* <div className="mt-2">
+            <CoffeBtn />
+          </div> */}
+
+          {/* <div className="mt-5">
+            <h1 className="text-xl font-bold">Templates</h1>
+            <div className="flex flex-row space-x-4 mt-2">
+              <TemplateSwitcher value={1} />
+              <TemplateSwitcher value={2} />
+              <TemplateSwitcher value={3} />
+            </div>
+          </div> */}
+        </div>
+      </motion.div>
+    </AnimatePresence>
+  );
+};
+
+export default SettingMain;
---
+++ data/cvData.js
@@ -0,0 +1,106 @@
+export const cvData = {
+  name: "Sandeep Baniya",
+  image: "",
+  jobTitle: "Hotel Manager",
+  location: "Urlabari, Morang",
+  email: "sandeep@gmail.com",
+  linkedIn: "",
+  instagram: "",
+  facebook: "",
+  twitter: "Sandeepism",
+  github: "+977-9814078552",
+  website: "",
+  about:
+    "After graduating from the Hotel Management department, I worked on a Five Star Hotel as a Chef for about 5 years. I have been working to be a Manager for the last 7 months and I am looking for a job in this field. Because of my old job",
+
+  toolsAndTechSkills: [
+    "Communication skills","Customer service skills","Teamwork","Time management","Attention to detail","Others",
+  ],
+  industryKnowledge: [
+    "Hospitality Sector Understanding","Customer Behavior","Trends and Practices","F&B Operations","Sales and Marketing","Others",
+  ],
+  languages: ["English","Nepali","Hindi","Spanish","Mandarin","French"],
+  skillTitle1: "Skills",
+  skillTitle2: "Industry Knowledge",
+  skillTitle3: "Languages",
+  projects: [
+    {
+      title: "CV Builder",
+      summary: `With this project you can create real time CV. You can see the changes you have made live. Built with Tailwind and contextAPI.`,
+    },
+    
+  ],
+  education: [
+    {
+      title: "Advance Diploma in Hotel Management",
+      school: "Tribhuwan University",
+      startDate: "2022",
+      endDate: "Now",
+    },
+    {
+      title: "Adhm with +2",
+      school: "Tribhuwan University",
+      startDate: "2017",
+      endDate: "2019",
+    },
+  ],
+  experiences: [
+    {
+      title: "Front Desk Staff",
+      company: "Hotel Yak & Yeti",
+      startDate: "2016",
+      endDate: "2020",
+      current: true,
+      summary: `It is an impartial information sharing platform about Syria. My job task:
+       Preparation of up-to-date maps according to the situation in the war zone. 
+       Preparing visual infographics about the obtained data and organizing these data. 
+       Adding articles and content to the site with Wordpress.`,
+    },
+   
+  ],
+  displayImage: true,
+  displayMail: true,
+  displayWebSite: true,
+  displayLinkedIn: false,
+  displayInstagram: false,
+  displayFacebook: false,
+  displayGithub: true,
+  displayTwitter: true,
+  activeColor: "#5B21B6",
+};
+
+/*
+export const cvDataEmpty = {
+  name: "",
+  jobTitle: "",
+  location: "",
+  email: "",
+  linkedin: "",
+  twitter: "",
+  github: "",
+  website: "",
+  aboutme: "",
+  toolsAndTechSkills: [],
+  industryKnowledge: [],
+  languages: [],
+  projects: [
+    {
+      title: "",
+      summary: "",
+    },
+  ],
+  experience: [
+    {
+      title: "",
+      company: "",
+      from: new Date(),
+      to: new Date(),
+      current: true,
+      summary: "",
+    },
+  ],
+  displayEducation: false,
+  displayProjects: false,
+  activeColor: "#5B21B6",
+};
+*/
